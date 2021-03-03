c816aa1b7b6039ad1d966d0f917c7130fbe52abf
erdongchen <568952575@qq.com>
2021-03-03 19:39:04 +0800

    20210303

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
 import Login from "../page/login.vue";
 import Manage from "../page/manage.vue";
 import OperatingIndex from "../page/operatingIndex.vue";
@@ -10,66 +10,61 @@ import WeChatOrders from "../page/weChatOrders.vue";
 import VipTime from "../page/vipTime.vue";
 import MobilePhone from "../page/mobilePhone.vue";
 Vue.use(VueRouter);
-const routes = [
-  {
+const routes = [{
     path: "/",
     name: "Login",
-    component: Login
-  },
-  {
-    path: '/',
-    component: OperatingIndex,
-    meta: ['运营指标'],
-  },
-  {
-    path: '/',
-    component: BasicInformation,
-    meta: ['基本信息'],
-  },
-  {
-    path: '/',
-    component: NoInvoice,
-    meta: ['未开发票'],
-  },
-  {
-    path: '/',
-    component: WeChatOrders,
-    meta: ['微信订单'],
-  },
-  {
-    path: '/',
-    component: VipTime,
-    meta: ['会员时间'],
-  },
-  {
-    path: '/',
-    component: MobilePhone,
-    meta: ['会员时间'],
-  },
-  {
-    path: "/",
-    name: "Manage",
-    component: Manage,
+    component: Login,
     children: [{
-      path: '/OperatingIndex',
-      component: OperatingIndex,
-      meta: ['运营指标'],
-    }]
+        path: "/",
+        name: "Manage",
+        component: Manage,
+      }, {
+        path: '/OperatingIndex',
+        component: OperatingIndex,
+        meta: ['运营指标'],
+      },
+      {
+        path: '/',
+        component: BasicInformation,
+        meta: ['基本信息'],
+      },
+      {
+        path: '/',
+        component: NoInvoice,
+        meta: ['未开发票'],
+      },
+      {
+        path: '/',
+        component: WeChatOrders,
+        meta: ['微信订单'],
+      },
+      {
+        path: '/',
+        component: VipTime,
+        meta: ['会员时间'],
+      },
+      {
+        path: '/',
+        component: MobilePhone,
+        meta: ['会员时间'],
+      },
+    ]
   },
-  {
-    path: "/home",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import( /* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  // },
+  // {
+  //   path: "/home",
+  //   name: "Home",
+  //   component: Home
+  // },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import( /* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
